803ac4b0eba3828f6819899491b7b670d43487e2
Sam Smets <samsmets@hotmail.com>
2021-05-28 10:57:40 +0800

    Added cors

--- app.js
+++ app.js
@@ -4,12 +4,22 @@ const app = express();
 
 require('dotenv').config();
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept",
+    );
+    req.header("Access-Control-Allow-Methods", "GET");
+    next();
+});
+
 app.get('/create-room', async (req, res) => {
     axios
         .post(`https://www.pokernow.club/new-integration-game?token=${process.env.INTEGRATION_TOKEN}`)
         .then(r => {
             if (r.data) {
-                res.send({ 'gameID': r.data.gameID });
+                res.send({ 'data': { 'gameID': r.data.gameID } });
             } else {
                 res.status(500).send({ 'error': 'something went wrong' });
             }
