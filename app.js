8cef5008b5cf09f1d1daf5559ebc6038a83af76c
Sadoge <Sadoge@users.noreply.github.com>
2021-06-01 07:19:42 +0000

    Deploying to gh-pages from @ Kosy-Software/create-poker-room@6c23545d9937b12b6c3ec6a661ebfd86973cd6e2 🚀

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+require('dotenv').config();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept",
+    );
+    req.header("Access-Control-Allow-Methods", "GET");
+    next();
+});
+
+app.get('/create-room', async (req, res) => {
+    axios
+        .post(`https://www.pokernow.club/new-integration-game?token=${process.env.INTEGRATION_TOKEN}`)
+        .then(r => {
+            if (r.data) {
+                res.send({ 'gameID': r.data.gameID });
+            } else {
+                res.status(500).send({ 'error': 'something went wrong' });
+            }
+        })
+        .catch(error => {
+            res.send({ 'error': error });
+        })
+});
+
+app.listen(8001, () => console.log('Server running on port 8001!'));
+
